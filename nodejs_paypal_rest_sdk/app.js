27ab26278e4d5c6142f63db1cdcca6cebf16f03d
Bilger Yahov <bilger@studyportals.com>
2017-11-11 15:15:39 +0100

    Set up simple Express JS server.

---
+++ nodejs_paypal_rest_sdk/app.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+
+const app = express();
+
+app.set('view_engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.listen(3000, () => {
+	console.log('Server started at port 3000...');
+});
+
